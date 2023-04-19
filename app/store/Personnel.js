f3cfbece405254de7d3ef74d8ab7389e64a093da
Atharva-Shelke <97449247+Atharva-Shelke@users.noreply.github.com>
2023-04-19 10:38:42 +0530

    Update Personnel.js

--- app/store/Personnel.js
+++ app/store/Personnel.js
@@ -6,10 +6,10 @@ Ext.define('MYAPP.store.Personnel', {
     model: 'MYAPP.model.Personnel',
 
     data: { items: [
-        { Series_id: '1', Series_name: "Ashes", TeamA: "India", TeamB:"Pakistan", Location:"Mohali", Time:"7:30 pm", Operation:"Delete/Update" },
-        { Series_id: '2', Series_name: "Champions Trophy",  TeamA: "India",  TeamB:"Bangladesh", Location:"Pune", Time:"7:30 pm", Operation:"Delete/Update"},
-        { Series_id: '3', Series_name: "XYZ", TeamA: "Srilanka", TeamB:"India", Location:"Indore", Time:"7:30 pm", Operation:"Delete/Update" },
-        { Series_id: '4', Series_name: "MYAPP", TeamA: "England", TeamB:"India", Location:"Delhi", Time:"7:30 pm", Operation:"Delete/Update" },
+        { Series_id: '1', Series_name: "India Tour of Australia", TeamA: "India", TeamB:"Australia", Location:"Mumbai", Time:"7:30 pm"},
+        { Series_id: '2', Series_name: "India Tour of New Zealand",  TeamA: "India",  TeamB:"New Zealand", Location:"Pune", Time:"1:30 pm"},
+        { Series_id: '3', Series_name: "India Tour of Srilanka", TeamA: "Srilanka", TeamB:"India", Location:"Indore", Time:"4:30 pm"},
+        { Series_id: '4', Series_name: "IPL", TeamA: "Royal Challengers Banglore", TeamB:"Kolkata Knight Riders", Location:"Kolkata", Time:"7:30 pm"},
     ]},
 
     proxy: {
